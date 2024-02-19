0af6d70ce783eb44563466f767c30086a96b04f0
jared-ortega <47844893+jared-ortega@users.noreply.github.com>
2024-02-18 21:09:43 -0800

    Express routing works

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path'); // Agrega esta línea
 
 app.get('/', (req, res) => {
-    res.send('¡Hola Mundo!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    console.log(`Running on ${PORT}`);
 });
