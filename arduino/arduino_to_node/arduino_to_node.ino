cd227c9785ecbe1d48b6acb3c4f196a2ae5d26c2
jared-ortega <47844893+jared-ortega@users.noreply.github.com>
2024-03-18 20:35:04 -0700

    Arduino code running

--- arduino/arduino_to_node/arduino_to_node.ino
+++ arduino/arduino_to_node/arduino_to_node.ino
@@ -1,24 +1,37 @@
+#include "HX711.h"
+const int DOUT=A1;
+const int CLK=A0;
+
 int percent = 0;
 int prevPercent = 0;
 
+HX711 balanza;
 void setup() {
+  Serial.begin(9600);
+  balanza.begin(DOUT, CLK);
+  // Serial.print("Lectura del valor del ADC:  ");
+  //Serial.println(balanza.read());
+  // Serial.println("No ponga ningun  objeto sobre la balanza");
+  // Serial.println("Destarando...");
+  // Serial.println("...");
+  balanza.set_scale(-102.45652); // Establecemos la escala
+  balanza.tare(20);  //El peso actual es considerado Tara.
   
-  Serial.begin( 9600 );
-  
+  //Serial.println("Listo para pesar");  
 }
 
 void loop() {
-  
-  percent = round(analogRead(0) / 1024.00 * 100);
-  
-  if(percent != prevPercent) {
-    
-    Serial.println(percent);
-    
-    prevPercent = percent;
-    
-  }
-  
-  delay(100);
+  // percent = round(analogRead(0) / 1024.00 * 100);
+  // if(percent != prevPercent) {
+  //   Serial.println(percent);
+  //   prevPercent = percent;
+  // }
+  // delay(100);
+  //Serial.print("Peso: ");
+  Serial.print(balanza.get_units(1),3);
+  Serial.println();
+  delay(1000);
+
+
   
 }
